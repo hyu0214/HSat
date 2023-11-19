2b081345bc39f45278e2aa6bd341a82945d0856b
Sangmin-dl <150664083+Sangmin-dl@users.noreply.github.com>
2023-11-19 15:24:43 +0900

    modified version

--- motor_pwm_test2.ino
+++ motor_pwm_test2.ino
@@ -11,48 +11,44 @@ void setup() {
   TCCR1A = bit(COM1A1) | bit(WGM11);  // Clear OC1A on compare match, set at TOP    Timing System - Clear Timer on Compare Match
   TCCR1B = bit(WGM13) | bit(WGM12) | bit(CS10);  // Fast PWM, no prescaler
   ICR1 = 799;  // TOP value for 20kHz PWM    PWM_frequency = clock_speed / [Prescaller_value * (1 + TOP_Value)]
+
+  Serial.begin(9600);
 }
+
+// 1st try
+//
+// void loop() {
+//   analogWrite(motorPin, 200);
+// }
+
 
 void loop() {
-  // Change the duty cycle
-  int dutyCycle = 799;  // Set duty cycle between 0 and 799
+  if (Serial.available() > 0) {
+    int dutyCycle = Serial.parseInt();
 
-  // Set the duty cycle by writing to OCR1A
-  OCR1A = dutyCycle;
+    dutyCycle = constrain(dutyCycle, 0, 799); // duty cycle value range
 
-  delay(500);
+    OCR1A = dutyCycle; // Set the duty cycle to write OCR1A
+
+    Serial.print("Duty Cycle set to: ");
+    Serial.println(dutyCycle);
+  }
+  delay(100);
 }
 
 
-//   analogWrite(motorPin, 200);
-//   // Serial.begin(9600);
-// }
-
 // void loop() {
+//   // Change the duty cycle
+//   int dutyCycle = 799;  // Set duty cycle between 0 and 799
 
-// }
+//   // Set the duty cycle by writing to OCR1A
+//   OCR1A = dutyCycle;
 
-// void loop() {
-//   if (Serial.available() > 0) {
-//     int dutyCycle = Serial.parseInt();
-
-//     dutyCycle = constrain(dutyCycle, 0, 799); // duty cycle value range
-
-//     OCR1A = dutyCycle; // Set the duty cycle to write OCR1A
-
-//     Serial.print("Duty Cycle set to: ");
-//     Serial.println(dutyCycle);
-//   }
-//   delay(100);
+//   delay(500);
 // }
 
 
 
-
-
-// void loop() {
-//   analogWrite(motorPin, 255);
-// }
 
 // void loop() {
 //   // Drive the motor from 0 to 255 sequentially
